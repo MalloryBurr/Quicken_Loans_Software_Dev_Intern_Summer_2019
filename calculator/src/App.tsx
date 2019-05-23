61b1b8250dfde89988d746ece654a0fbe37dcef3
Mallory Burr <malloryburr26@gmail.com>
2019-05-23 16:53:56 -0400

    Working buttons

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -16,20 +16,29 @@ class App extends React.Component <{}, IAppState> {
     return (
       <div style={{textAlign: 'center'}}>
         <CalcOutput calc={this.state.result} />
-        <button onClick={this.oneHandler}>1</button>
+        <button onClick={this.oneHandler} id="1" >1</button>
+        <button onClick={this.oneHandler} id="2" >2</button>
+        <button onClick={this.oneHandler} id="3" >3</button>
+        <br /> <br/>
         <button onClick={this.addHandler}>add</button>
         
       </div>
     );  
   }
 
-  public oneHandler = () => {
-      return this.setState( { num1: 10 } );
+  public oneHandler = (event: any) => {
+      // /* tslint:disable */
+      // console.log(event.target.id);
+      return this.setState( { num1: parseInt(event.target.id, 0) } );
   };
 
   public addHandler = () => {
       return this.setState( {result: this.state.num1 + this.state.num2} );
   };
+
+  // public deleteRow = (id, e) => {
+  //   return this.setState { id: 1}
+  // }
 
 
 }
