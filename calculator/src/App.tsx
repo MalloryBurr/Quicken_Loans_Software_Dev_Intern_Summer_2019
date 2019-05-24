fb8335859153a32525ca54c22952b2767ccd18ef
Mallory Burr <malloryburr26@gmail.com>
2019-05-24 12:38:29 -0400

    Working calc

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -7,39 +7,46 @@ import * as React from 'react';
 interface IAppState {
   num1: number;
   num2: number;
+  action: string;
   result: number;
   count: number;
 }
 
 class App extends React.Component <{}, IAppState> {
-  public state = {num1: 0, num2: 0, result: 0, count: 1}
+  public state = {num1: 0, num2: 0, result: 0, count: 1, action: ""}
 
   public render() {
     
     return (
-      <div style={{textAlign: 'center'}}>
-        <p>
-        {this.state.num1}   &nbsp;  {this.state.num2} <br/>
-        result:     {this.state.result}<br/><br/>
+      <div id = "calc">
+        <p id="display">
+          {this.state.num1} {this.state.action} {this.state.num2}<br/>
+          {this.state.result}
         </p>
+      
         
 
         <button onClick={this.numHandler} id="1" >1</button>
         <button onClick={this.numHandler} id="2" >2</button>
         <button onClick={this.numHandler} id="3" >3</button>
-        <br /> <br/>
+        <br/> 
         <button onClick={this.numHandler} id="4" >4</button>
         <button onClick={this.numHandler} id="5" >5</button>
         <button onClick={this.numHandler} id="6" >6</button>
-        <br /> <br/>
+        <br/> 
         <button onClick={this.numHandler} id="7" >7</button>
         <button onClick={this.numHandler} id="8" >8</button>
         <button onClick={this.numHandler} id="9" >9</button>
-        <br /> <br/>
-        <button onClick={this.addHandler}>+</button>
-        <button onClick={this.subHandler}>-</button>
-        <button onClick={this.mulHandler}>*</button>
-        <button onClick={this.divHandler}>/</button>
+        <br/> 
+
+        <div id="actButtons">
+          <button onClick={this.actHandler} id="+">+</button> 
+          <button onClick={this.actHandler} id="-">-</button><br/>
+          <button onClick={this.actHandler} id="*">*</button>
+          <button onClick={this.actHandler} id="/">/</button><br/>
+        </div>
+
+        <button onClick={this.resHandler} id="res">=</button>
         
       </div>
     );  
@@ -58,35 +65,35 @@ class App extends React.Component <{}, IAppState> {
     else{
       this.setState ({
         num2: parseInt(event.target.id, 0),
-        count: 1
+        count: 1,
       });
+    } 
+  };
+
+  public actHandler = (event: any) => { 
+    this.setState( {
+      action: event.target.id
+    } );
+  };
+
+  public resHandler = () => { 
+    switch(this.state.action) {
+      case "+":
+        this.setState( { result: this.state.num1 + this.state.num2 });
+        break;
+
+      case "-":
+        this.setState( { result: this.state.num1 - this.state.num2 });
+        break;
+
+      case "*":
+        this.setState( { result: this.state.num1 * this.state.num2 });
+        break;
+
+      case "/":
+        this.setState( { result: this.state.num1 / this.state.num2 });
+        break;
     }
-
-        
-  };
-
-  public addHandler = () => { 
-    this.setState( {
-      result: this.state.num1 + this.state.num2,
-    } );
-  };
-
-  public subHandler = () => { 
-    this.setState( {
-      result: this.state.num1 - this.state.num2,
-    } );
-  };
-
-  public mulHandler = () => { 
-    this.setState( {
-      result: this.state.num1 * this.state.num2,
-    } );
-  };
-
-  public divHandler = () => { 
-    this.setState( {
-      result: this.state.num1 / this.state.num2,
-    } );
   };
 }
 
